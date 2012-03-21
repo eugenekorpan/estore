0277c3c83995defbaf8967314c4958a200b569bc
Eugene Korpan <eugene@svitla.com>
2012-03-21 10:52:40 +0200

    First commit - empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Estore::Application.config.secret_token = '63ed7eabcd63dd596ddd32b1143e6b0d133d1b385c8eb5ccd063abfc245d24bcecab010bc3f142020129e1dd48c4e141825d45093c2496ce51e47125368ad59d'
